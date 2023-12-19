607d16518d7b7cda9161895a18dd2b686fbdb8bd
laithsleit <laithsleit@gmail.com>
2023-12-20 00:43:56 +0300

    12:42

--- New folder (3)/checkout/checkout.js
+++ New folder (3)/checkout/checkout.js
@@ -1,132 +1,111 @@
 // Function to fetch and display cart items
 function fetchCartItemsAndDisplay(userID) {
-    fetch(`http://localhost/api-AMM/api/cart/cart-api.php?UserID=${userID}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
+    fetch(`http://localhost/api-AMM/api/cart/cart-api.php?UserID=${userID}`)
         .then(response => response.json())
         .then(data => {
             const productsCards = document.getElementById('products-cards');
             productsCards.innerHTML = ''; // Clear existing items
-            data.data.forEach(item => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                <td>
-                    <button class="delete-btn" data-product-id="${item.ProductID}">
-                        <i class="fas fa-trash-alt"></i> <!-- Trash icon -->
-                    </button>
-                </td>
-                <td> <img src="${item.Image}" > </td>
-                <td> ${item.ProductName} </td>
-                <td> ${item.Price} </td>
-                <td> ${item.Quantity} </td>
-            `;
-                productsCards.appendChild(row);
 
-                // Add click event listener to delete button
-                row.querySelector('.delete-btn').addEventListener('click', () => {
-                    deleteCartItem(item.ProductID);
+            if (data && Array.isArray(data.data)) {
+                data.data.forEach(item => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>
+                            <button class="delete-btn" data-product-id="${item.ProductID}">
+                                <i class="fas fa-trash-alt"></i> <!-- Trash icon -->
+                            </button>
+                        </td>
+                        <td> <img src="${item.Image}" alt="Product Image"> </td>
+                        <td> ${item.ProductName} </td>
+                        <td> $${item.Price} </td>
+                        <td> ${item.Quantity} </td>
+                    `;
+                    productsCards.appendChild(row);
                 });
-            });
+
+                document.querySelectorAll('.delete-btn').forEach(button => {
+                    button.addEventListener('click', function() {
+                        const productId = this.getAttribute('data-product-id');
+                        deleteCartItem(productId, userID);
+                    });
+                });
+            }
         })
         .catch(error => {
             console.error('Error fetching cart data:', error);
         });
 }
 
-// Function to delete a cart item
-function deleteCartItem(productId) {
-    const userId = sessionStorage.getItem('UserID');
+// Function to delete a cart item and update the display
+function deleteCartItem(productId, userID) {
+    fetch(`http://localhost/api-AMM/api/cart/cart-api.php?UserID=${userID}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ProductID: productId,
+            action: 'delete'
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // After deleting the cart item, update the product and price tables
+            updateProductTable(productId);
+            updatePriceTable(userID);
 
-    fetch(`http://localhost/api-AMM/api/cart/cart-api.php?UserID=${userId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ProductID: productId,
-                action: 'delete'
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log('Item deleted successfully');
-                updateCartDisplay();
-            } else {
-                throw new Error(data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error deleting cart item:', error);
-        });
+            fetchCartItemsAndDisplay(userID);
+            updateCartDetails(userID);
+        } else {
+            throw new Error(data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error deleting cart item:', error);
+    });
 }
 
-// Function to update the cart display
-function updateCartDisplay() {
-    const userID = sessionStorage.getItem('UserID');
-    fetchCartItemsAndDisplay(userID);
-    updateCartDetails();
+function updateProductTable(productId) {
+    // API call to update the product table
+    // Placeholder for actual implementation
+    console.log(`Updating product table for ProductID: ${productId}`);
+    // Implement the API call here
 }
 
-// Function to fetch the total cart price
-function fetchTotalCartPrice(userID) {
-    return fetch(`http://localhost/api-AMM/api/cart/totalcart.php?UserID=${userID}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Total Cart data:', data);
-            return data.data; // Assuming the total price is in the "data" field
-        })
-        .catch(error => {
-            console.error('Error fetching total cart data:', error.message);
-            return null;
-        });
+function updatePriceTable(userID) {
+    // API call to update the price table
+    // Placeholder for actual implementation
+    console.log(`Updating price table for UserID: ${userID}`);
+    // Implement the API call here
 }
 
 // Function to update the HTML with the cart details
-function updateCartDetails() {
-    const userID = sessionStorage.getItem('UserID');
-    fetchTotalCartPrice(userID)
-        .then(totalCartPrice => {
-            if (totalCartPrice !== null) {
-                const cartDetailsDiv = document.getElementById('subtotal');
-
-                // Update the HTML content with the retrieved data
+function updateCartDetails(userID) {
+    fetch(`http://localhost/api-AMM/api/cart/totalcart.php?UserID=${userID}`)
+        .then(response => response.json())
+        .then(data => {
+            const cartDetailsDiv = document.getElementById('subtotal');
+            if (data.success && data.data) {
                 cartDetailsDiv.innerHTML = `
-                        <table>
-                            <tr>
-                                <td>Cart Subtotal</td>
-                                <td>${totalCartPrice} JD</td>
-                            </tr>
-                            <tr>
-                                <td>Shipping</td>
-                                <td>Free</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Total</strong></td>
-                                <td><strong>${totalCartPrice} JD</strong></td>
-                            </tr>
-                        </table>
-                        <a href="../dist-payment/payment.html"><button id="check">Proceed to checkout</button></a>
-                    `;
+                    <table>
+                        <tr>
+                            <td>Cart Subtotal</td>
+                            <td>$${data.data} JD</td>
+                        </tr>
+                        <tr>
+                            <td>Shipping</td>
+                            <td>Free</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Total</strong></td>
+                            <td><strong>$${data.data} JD</strong></td>
+                        </tr>
+                    </table>
+                    <a href="../dist-payment/payment.html"><button id="check">Proceed to checkout</button></a>
+                `;
             } else {
-                // Handle the case when totalCartPrice is null
-                const cartDetailsDiv = document.getElementById('subtotal');
-                cartDetailsDiv.innerHTML = `
-                        <p>Your cart is empty</p>
-                    `;
+                cartDetailsDiv.innerHTML = `<p>Your cart is empty</p>`;
             }
         })
         .catch(error => {
@@ -134,20 +113,19 @@ function updateCartDetails() {
         });
 }
 
-
-// Call the function to update cart details
-updateCartDisplay();
-
 // Event listener for DOMContentLoaded
 document.addEventListener("DOMContentLoaded", function() {
-    const isLoggedIn = sessionStorage.getItem("isLoggedin") === "true";
-    const loginButton = document.getElementById('Login');
+    const userID = sessionStorage.getItem('UserID');
+    if (userID) {
+        fetchCartItemsAndDisplay(userID);
+        updateCartDetails(userID);
+    }
 
-    if (isLoggedIn) {
-        // If the user is logged in, change button text to "Logout" and set the click event
+    // Login button event listener
+    const loginButton = document.getElementById('Login');
+    if (sessionStorage.getItem("isLoggedin") === "true") {
         loginButton.textContent = 'Logout';
         loginButton.href = '#';
-
         loginButton.addEventListener('click', function() {
             sessionStorage.removeItem("isLoggedin");
             window.location.href = 'signup.html';
@@ -156,4 +134,13 @@ document.addEventListener("DOMContentLoaded", function() {
         loginButton.textContent = 'Login';
         loginButton.href = 'signup.html';
     }
+
+    // Checkout button event listener
+    document.querySelector('.checkout-btn').addEventListener('click', function() {
+        if (sessionStorage.getItem('isLoggedin') === 'true') {
+            window.location.href = '../checkout/checkout.html';
+        } else {
+            window.location.href = '../login/signup.html';
+        }
+    });
 });
