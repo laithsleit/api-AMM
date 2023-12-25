bdd138543cd998c559c4ecfd6367871c1c50ecdb
laithsleit <laithsleit@gmail.com>
2023-12-25 20:46:33 +0300

    .

--- api/prodect/update.php
+++ api/prodect/update.php
@@ -36,6 +36,14 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json");
+    exit();
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     function updateProduct($data)
     {
--- api/user/update.php
+++ api/user/update.php
@@ -5,12 +5,20 @@
  */
 
 // Include the database connection file
-include_once "../include.php"; // Make sure this file contains the correct database connection setup
+include_once "../include.php";
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: PUT, GET,PATCH, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json");
+    exit();
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
 
